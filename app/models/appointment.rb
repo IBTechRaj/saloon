cbb9550590fe1dfbbd715512fef09ca07bf4d0c5
IBTechRaj <raj_shk@rediffmail.com>
2021-10-08 10:25:20 +0530

    more validations done

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -2,4 +2,8 @@ class Appointment < ApplicationRecord
   belongs_to :user
   belongs_to :saloonspas 
   belongs_to :client
+
+  validates :apptdate, presence: true, inclusion: { in: (Date.today+1..Date.today+7.days) }
+  validates :appttime, :on_or_after => '6:00am', :on_or_before => '10:00am'
+  validates :service, presence: true
 end
--- app/models/worktime.rb
+++ app/models/worktime.rb
@@ -3,5 +3,5 @@ class Worktime < ApplicationRecord
 
 
   validates :weekday,  numericality: { greater_than_or_equal_to: 1, less_than_or_equal_to: 7 }, presence: true
-  validates :opens, :on_or_after => '6:00am', :on_or_before => '10:00am',
+  validates :opens, :on_or_after => '6:00am', :on_or_before => '10:00am'
 end
