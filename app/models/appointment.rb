c05b63889880b8e5e4cc753a980c50e43cc971f4
IBTechRaj <raj_shk@rediffmail.com>
2021-10-08 12:28:13 +0530

    some validation errors fixed

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -4,6 +4,5 @@ class Appointment < ApplicationRecord
   belongs_to :client
 
   validates :apptdate, presence: true, inclusion: { in: (Date.today+1..Date.today+7.days) }
-  validates :appttime, :on_or_after => '6:00am', :on_or_before => '10:00am'
   validates :service, presence: true
 end
--- app/models/worktime.rb
+++ app/models/worktime.rb
@@ -3,5 +3,4 @@ class Worktime < ApplicationRecord
 
 
   validates :weekday,  numericality: { greater_than_or_equal_to: 1, less_than_or_equal_to: 7 }, presence: true
-  validates :opens, :on_or_after => '6:00am', :on_or_before => '10:00am'
-end
+  end
